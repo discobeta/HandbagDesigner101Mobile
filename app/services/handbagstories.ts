92c86589e72e722ea2fe340a22d102e5e592543c
Asaf Klibansky <discobeta@gmail.com>
2016-05-16 13:47:54 -0400

    added story service to fetch stories from handbagdesigner101.com api point

---
+++ app/services/handbagstories.ts
@@ -0,0 +1,14 @@
+import {Injectable} from 'angular2/core';  
+import {Http, Headers} from 'angular2/http';
+
+@Injectable()
+export class HandbagService {  
+    constructor(private http: Http) {
+    }
+
+    getStories(page,count,type) {
+        let stories = this.http.get(`https://handbagdesigner101.com/story/mobile/${page}/${count}/${type}`);
+        // check if we have cache
+        return stories;
+    }
+}

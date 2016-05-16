e3ec10dce515f18470da2c050a498b8ec4f5abb9
Asaf Klibansky <discobeta@gmail.com>
2016-05-16 16:11:23 -0400

    prototyped html and typescript to access the new story service and populate items, added infinite scroll feature that stops at 90 records

--- app/pages/page1/page1.ts
+++ app/pages/page1/page1.ts
@@ -1,4 +1,4 @@
-import {Page} from 'ionic-angular';
+import {Page, InfiniteScroll} from 'ionic-angular';
 import {HandbagService} from '../../services/handbagstories';
 
 @Page({
@@ -15,15 +15,60 @@ export class Page1 {
   public page = 1
   public count = 20
   public storyType = 'designer'
+  public temporaryStories = []
+
+  goToStory(link) {
+  	window.open('http://handbagdesigner101.com'+link, '_self');
+  }
+
+  loadMoreStories() {
+    this._service.getStories(this.page,this.count,this.storyType).subscribe(
+        data => {
+            this.foundStories = data.json()
+        },
+        err => console.error(err),
+        () => console.log('loadMoreStories completed')
+    )
+    if (this.foundStories[19]) {
+    	this.page = this.page + 1
+    } else {
+    	this.page = 1
+    }
+    
+  }
+
+  doInfinite(infiniteScroll: InfiniteScroll) {
+    this._service.getStories(this.page,this.count,this.storyType).subscribe(
+        data => {
+        	this.temporaryStories = data.json();
+        },
+        err => console.log(err),
+        () => console.log('loadMoreStories completed')
+    )
+
+	for (var i = 0; i < this.temporaryStories.length; i++) {
+		this.foundStories.push( this.temporaryStories[i] );
+	}
+	this.page = this.page + 1;
+    
+    infiniteScroll.complete();
+
+	if (this.foundStories.length > 90) {
+		infiniteScroll.enable(false);
+	}
+    
+  }
 
   onPageWillEnter() {
+  	this.page = 1
     this._service.getStories(this.page,this.count,this.storyType).subscribe(
         data => {
             this.foundStories = data.json();
         },
-        err => console.error(err),
+        err => console.log(err),
         () => console.log('getStories completed')
     );
+    
   }
 
 }

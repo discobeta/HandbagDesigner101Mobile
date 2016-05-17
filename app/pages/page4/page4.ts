7af097c0e4baa355d6f008ecdc4f5abc221ee0b0
Asaf Klibansky <discobeta@gmail.com>
2016-05-17 07:34:04 -0400

    added a new tab - celebrity, added new styling for tabs

---
+++ app/pages/page4/page4.ts
@@ -0,0 +1,72 @@
+import {Page, InfiniteScroll} from 'ionic-angular';
+import {HandbagService} from '../../services/handbagstories';
+
+@Page({
+  templateUrl: 'build/pages/page4/page4.html',
+  providers: [HandbagService]
+})
+export class Page4 {
+
+  constructor(private _service: HandbagService) {
+  }
+  public foundStories
+  public page = 1
+  public count = 20
+  public storyType = 'celebrity'
+  public temporaryStories = []
+
+  goToStory(link) {  
+    window.open('http://handbagdesigner101.com'+link, '_blank', 'location=yes');
+  }
+
+  loadMoreStories() {
+    this._service.getStories(this.page,this.count,this.storyType).subscribe(
+        data => {
+            this.foundStories = data.json()
+        },
+        err => console.error(err),
+        () => console.log('loadMoreStories completed')
+    )
+    if (this.foundStories[19]) {
+    	this.page = this.page + 1
+    } else {
+    	this.page = 1
+    }
+    
+  }
+
+  doInfinite(infiniteScroll: InfiniteScroll) {
+    this._service.getStories(this.page,this.count,this.storyType).subscribe(
+        data => {
+        	this.temporaryStories = data.json();
+        },
+        err => console.log(err),
+        () => console.log('loadMoreStories completed')
+    )
+
+	for (var i = 0; i < this.temporaryStories.length; i++) {
+		this.foundStories.push( this.temporaryStories[i] );
+	}
+	this.page = this.page + 1;
+    
+    infiniteScroll.complete();
+
+	if (this.foundStories.length > 90) {
+		infiniteScroll.enable(false);
+	}
+    
+  }
+
+  onPageWillEnter() {
+  	this.page = 1
+    this._service.getStories(this.page,this.count,this.storyType).subscribe(
+        data => {
+            this.foundStories = data.json();
+        },
+        err => console.log(err),
+        () => console.log('getStories completed')
+    );
+    
+  }
+
+}
--- app/pages/tabs/tabs.ts
+++ app/pages/tabs/tabs.ts
@@ -2,7 +2,7 @@ import {Page} from 'ionic-angular';
 import {Page1} from '../page1/page1';
 import {Page2} from '../page2/page2';
 import {Page3} from '../page3/page3';
-
+import {Page4} from '../page4/page4';
 
 @Page({
   templateUrl: 'build/pages/tabs/tabs.html'
@@ -13,4 +13,5 @@ export class TabsPage {
   tab1Root: any = Page1;
   tab2Root: any = Page2;
   tab3Root: any = Page3;
+  tab4Root: any = Page4;
 }
